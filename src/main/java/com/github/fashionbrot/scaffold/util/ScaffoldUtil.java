64a1cfd3523a29e8c75a7c5e36b887296e63020f
国然 <fashionbrot@163.com>
2020-08-24 14:29:55 +0800

    优化

--- src/main/java/com/github/fashionbrot/scaffold/util/ScaffoldUtil.java
+++ src/main/java/com/github/fashionbrot/scaffold/util/ScaffoldUtil.java
@@ -310,10 +310,13 @@ public class ScaffoldUtil {
      * @return
      */
     private static String captureName(String str) {
-        // 进行字母的ascii编码前移，效率要高于截取字符串进行转换的操作
-        char[] cs=str.toCharArray();
-        cs[0]-=32;
-        return String.valueOf(cs);
+        if (StringUtils.isNotBlank(str)) {
+            // 进行字母的ascii编码前移，效率要高于截取字符串进行转换的操作
+            char[] cs = str.toCharArray();
+            cs[0] -= 32;
+            return String.valueOf(cs);
+        }
+        return "";
     }
 
 }
